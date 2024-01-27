1c0b04e2e778b33abc6d29369452ca0f677a435f
SONALI SAHANI <sonnaa.sahani@gmail.com>
2024-01-27 07:28:58 +0000

    connect to db initiate

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+
+//Connect to mongodb
+connectDB();
+
 const app = express();
+//Enabling CORS here for saftey of access into from other domains
+app.use(cors());
+//Handle JSON parsing properly
+app.use(express.json())
 const port = process.env.PORT || 5000;
 app.listen(port, () =>
 console.log(`server is running on port ${port}`)
-    ); 
+   );  
