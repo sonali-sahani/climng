4cce5d57204153fdaea9e0e255b2157b809bbe4a
SONALI SAHANI <sonnaa.sahani@gmail.com>
2024-01-27 08:13:14 +0000

    create a install db

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
+const patients= require("./routes/api/patients");
 
 //Connect to mongodb
 connectDB();
@@ -8,8 +9,11 @@ connectDB();
 const app = express();
 //Enabling CORS here for saftey of access into from other domains
 app.use(cors());
+
 //Handle JSON parsing properly
 app.use(express.json())
+//use the api group instead of muliple path for multiple routes
+app.use("/api/patients", patients)
 const port = process.env.PORT || 5000;
 app.listen(port, () =>
 console.log(`server is running on port ${port}`)
