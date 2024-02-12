7653673982d6857c01ef898446ba48b59292fe1e
SONALI SAHANI <sonnaa.sahani@gmail.com>
2024-02-12 07:37:49 +0000

    add react toster,connect mongodb with link

--- client/src/components/CreatePatient.js
+++ client/src/components/CreatePatient.js
@@ -1,31 +1,45 @@
-// import React, { useState } from 'react';
+ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import ShowPatientList from './ShowPatientList';
 import Navbar from './navbar';
 import Footer from './footer';
-
+ 
 
 const CreatePatient = (props) => {
   // Define the state with useState hook
+  const navigate = useNavigate();
+  const [patient, setPatient] = useState({
+    Patient_id: '',
+    Name: '',
+    Address: '',
+    Phone_no: '',
+    Gender: '',
+    Age: '',
+  });
+  const [showToast, setShowToast] = useState(false);
+
+  const onChange = (e) => {
+    setPatient({ ...patient, [e.target.name]: e.target.value });
+  };
   const onSubmit = (e) => {
    // prevent the page from refreshing
     e.preventDefault();
     //API calls with the help of axios
-    // axios
-    //   .post('/api/patients', patient)
-    //   .then((res) => {
-    //     setPatient({
-    //       patient_id: '',
-    //       name: '',
-    //       address: '',
-    //       phone_no: '',
-    //       gender: '',
-    //       age: '',
-    //       join_date: '',
-    //     });
-    //   })
+    axios
+      .post('/api/patients', patient)
+      .then((res) => {
+        setPatient({
+          patient_id: '',
+          name: '',
+          address: '',
+          phone_no: '',
+          gender: '',
+          age: '',
+          join_date: '',
+        });
+      })
       
     }
 
@@ -48,14 +62,70 @@ const CreatePatient = (props) => {
               <div className='form-group'>
                 <input
                   type='number'
-                  placeholder='Enter Your Name'
-                  address='Enter Your Address'
+                  placeholder='Enter Your ID'
+                  name='patient_id'
                   className='form-control'
-                  // value={patient.name}
-                  // onChange={onChange}
+                  value={patient.id}
+                  onChange={onChange}
                 />
                 
             </div>
+            <div className='form-group'>
+                <input
+                  type='text'
+                  placeholder='Enter Your Name'
+                  name='patient_name'
+                  className='form-control'
+                  value={patient.name}
+                  onChange={onChange}
+                />
+              </div>
+
+              <div className='form-group'>
+                <input
+                  type='text'
+                  placeholder='Enter Your Address'
+                  name='patient_address'
+                  className='form-control'
+                  value={patient.address}
+                  onChange={onChange}
+                />
+              </div>
+
+              <div className='form-group'>
+                <input
+                  type='number'
+                  placeholder='Enter Your Phone.no'
+                  name='patient_phnno.'
+                  className='form-control'
+                  value={patient.phnno}
+                  onChange={onChange}
+                />
+              </div>
+
+              <div className='form-group'>
+                <input
+                  type='text'
+                  placeholder='Enter Your Gender'
+                  name='patient_gender'
+                  className='form-control'
+                  value={patient.gender}
+                  onChange={onChange}
+                />
+              </div>
+              <div className='form-group'>
+                <input
+                  type='number'
+                  placeholder='Enter Your Age'
+                  name='patient_age'
+                  className='form-control'
+                  value={patient.age}
+                  onChange={onChange}
+                />
+              </div>
+
+
+
             </form>
             
               </div>
