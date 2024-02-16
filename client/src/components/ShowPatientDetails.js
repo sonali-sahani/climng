a603183b4cef1ea77794f4e543b8287a6f633a29
SONALI SAHANI <sonnaa.sahani@gmail.com>
2024-02-16 08:19:43 +0000

    patientdetails, patient info partial done

--- client/src/components/ShowPatientDetails.js
+++ client/src/components/ShowPatientDetails.js
@@ -1,23 +1,167 @@
-// // import React, { useState } from 'react';
-// // import { Link } from 'react-router-dom';
-// // import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Link, useParams, useNavigate } from 'react-router-dom';
+import { Slide, ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import '../App.css';
+import axios from 'axios';
+import footer from './footer';
+import navbar from './navbar';
 
-// // import { useNavigate } from 'react-router-dom';
+function ShowPatientDetails(props) {
+  const [patient, setPatient] = useState({});
+  // const [showToast, setShowToast] = useState(false);
 
-// const ShowPatientDetails = (props) => {
-//     // Define the state with useState hook
-   
-  
-//     return (
-//       <div className='ShowPatientDetails'>
-//         <div className='container'>
-//           <div className='row'>
-//             <h1>new patient</h1>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   };
-  
-//   export default ShowPatientDetails;
-  
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get(`/api/patients/${id}`)
+      .then((res) => {
+        setPatient(res.data);
+      })
+      .catch((err) => {
+        console.log('Error from ShowPatientDetails');
+      });
+  }, [id]);
+
+  const onDeleteClick = (id) => {
+    axios
+      .delete(`/api/patients/${id}`)
+      .then((res) => {
+
+        // Show the success alert
+        toast.success('Patient deleted!', {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+          transition: Slide,
+        });
+
+        // Delay the navigation slightly to allow the toast to be seen
+        setTimeout(() => {
+          // setShowToast(false); // Hide the toast
+          navigate('/'); // Navigate to homepage
+        }, 5000); // Adjust the timeout as needed
+      })
+      .catch((err) => {
+        console.log('Error in CreatePatient!');
+        console.log('The error is -> ')
+        console.log(err)
+        // Show the success alert
+        toast.error('Error while deleting the patient, please try again!', {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+          transition: Slide,
+        });
+      });
+  };
+
+  return (
+    <div className='ShowPatientDetails'>
+      <navbar />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Slide}
+      />
+
+      <div className='container'>
+        <div className='row justify-content-center'>
+          <div className='col-md-8'>
+            <h1 className='display-4 text-center'>Patient's Record</h1>
+            <p className='lead text-center'>View Patient's Info</p>
+            <hr /> <br />
+
+            <div className='row justify-content-center'>
+              <div className='col-md-12'>
+                <table className='table table-striped table-bordered table-dark'>
+                  <tbody>
+                    <tr>
+                      <th scope='row'>Patient_id</th>
+                      <td>{patient.patient_id}</td>
+                    </tr>
+                    <tr>
+                      <th scope='row'>Name</th>
+                      <td>{patient.name}</td>
+                    </tr>
+                    <tr>
+                      <th scope='row'>Address</th>
+                      <td>{patient.address}</td>
+                    </tr>
+                    <tr>
+                      <th scope='row'>Phone_no</th>
+                      <td>{patient.phone_no}</td>
+                    </tr>
+                    <tr>
+                      <th scope='row'>Gender</th>
+                      <td>{patient.gender}</td>
+                    </tr>
+                    <tr>
+                      <th scope='row'>Age</th>
+                      <td>{patient.age}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+
+            <div className='row justify-content-around mt-3'>
+              <div className='col-md-4'>
+                <button
+                  type='button'
+                  className='btn btn-outline-danger btn-lg btn-block'
+                  onClick={() => {
+                    onDeleteClick(patient._id);
+                  }}
+                >
+                  Delete patient
+                </button>
+              </div>
+              <div className='col-md-4'>
+                <Link
+                  to={`/edit-patient/${patient._id}`}
+                  className='btn btn-outline-info btn-lg btn-block'
+                >
+                  Edit patient
+                </Link>
+              </div>
+              <div className='col-md-4'>
+                <Link to='/' className='btn btn-outline-warning btn-lg btn-block'>
+                  Show Patient List
+                </Link>
+              </div>
+            </div>
+
+
+
+
+          </div>
+        </div>
+      </div>
+
+      <footer />
+    </div>
+  );
+}
+
+export default ShowPatientDetails;
--- client/src/components/UpdatePatientInfo.js
+++ client/src/components/UpdatePatientInfo.js
@@ -1,23 +1,196 @@
-// import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Link, useParams, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import '../App.css';
+import navbar from './navbar';
+import footer from './footer';
 
-// import { useNavigate } from 'react-router-dom';
+function UpdatePatientInfo(props) {
+  const [patient, setPatient] = useState({
+    patient_id: '',
+    name: '',
+    address: '',
+    phone_no: '',
+    gender: '',
+    age: '',
+    join_date: '',
+  });
 
-const UpdatePatientInfo = (props) => {
-    // Define the state with useState hook
-   
-  
-    return (
-      <div className='UpdatePatientInfo'>
-        <div className='container'>
-          <div className='row'>
-            <h1>new patient</h1>
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get(`/api/patients/${id}`)
+      .then((res) => {
+        setPatient({
+          patient_id: res.data.patient_id,
+          name: res.data.name,
+          address:res.data.address,
+          phone_no: res.data.phone_no,
+          gender: res.data. gender,
+          age: res.data.age,
+          join_date: res.data.join_date,
+        });
+      })
+      .catch((err) => {
+        console.log('Error from UpdatePatientInfo GET request');
+        console.log(err)
+      });
+  }, [id]);
+
+  const onChange = (e) => {
+    setPatient({ ...patient, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const data = {
+      patient_id: patient.patient_id,
+      name:  patient.name,
+      address: patient.address,
+      phone_no:  patient.phone_no,
+      gender:  patient. gender,
+      age:  patient.age,
+      join_date:  patient.join_date,
+    };
+
+    axios
+      .put(`/api/patients/${id}`, data)
+      .then((res) => {
+        navigate(`/show-patient/${id}`);
+      })
+      .catch((err) => {
+        console.log('Error in UpdatePatientInfo PUT request ->');
+        console.log(err)
+      });
+  };
+
+  return (
+    <div className='UpdatePatientInfo'>
+      <navbar />
+      
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-8 m-auto'>
+            <br />
+            <Link to='/' className='btn btn-outline-warning float-left'>
+              Show Patient List
+            </Link>
+          </div>
+          <div className='col-md-8 m-auto'>
+            <h1 className='display-4 text-center'>Edit Patient</h1>
+            <p className='lead text-center'>Update Patient's Info</p>
           </div>
         </div>
+
+        <div className='col-md-8 m-auto'>
+          <form noValidate onSubmit={onSubmit}>
+            <div className='form-group'>
+              <label htmlFor='patient_id'>Patient_id</label>
+              <input
+                type='Number'
+                placeholder='Enter Your ID'
+                name='patient_id'
+                className='form-control'
+                value={patient.patient_id}
+                onChange={onChange}
+              />
+            </div>
+            <br />
+
+            <div className='form-group'>
+              <label htmlFor='name'>Name</label>
+              <input
+               type='text'
+               placeholder='Enter Your Name'
+               name='name'
+               className='form-control'
+               value={patient.name}
+               onChange={onChange}
+              />
+            </div>
+            <br />
+
+            <div className='form-group'>
+              <label htmlFor='address'>Address</label>
+              <input
+                 type='text'
+                 placeholder='Enter Your Address'
+                 name='address'
+                 className='form-control'
+                 value={patient.address}
+                 onChange={onChange}
+              />
+            </div>
+            <br />
+
+            <div className='form-group'>
+              <label htmlFor='phone_no'>Phone_no</label>
+              <textarea
+               type='Number'
+               placeholder='Enter Your Phone.no'
+               name='phone_no'
+               className='form-control'
+               value={patient.phone_no}
+               onChange={onChange}
+              />
+            </div>
+            <br />
+
+            <div className='form-group'>
+              <label htmlFor='gender'>Gender</label>
+              <input
+                type='text'
+                placeholder='Enter Your Gender'
+                name='gender'
+                className='form-control'
+                value={patient.gender}
+                onChange={onChange}
+              />
+            </div>
+            <br />
+
+            <div className='form-group'>
+              <label htmlFor='age'>Age</label>
+              <input
+               type='Number'
+               placeholder='Enter Your Age'
+               name='age'
+               className='form-control'
+               value={patient.age}
+               onChange={onChange}
+              />
+            </div>
+            <br />
+            <div className='form-group'>
+              <label htmlFor='join_date'>Join_date</label>
+              <input
+               type='Date'
+               placeholder='Enter Your Join-Date'
+               name='join_date'
+               className='form-control'
+               value={patient.join_date}
+               onChange={onChange}
+              />
+            </div>
+            <br />
+
+            <button
+              type='submit'
+              className='btn btn-outline-info btn-lg btn-block'
+            >
+              Update Patient
+            </button>
+            <br /> <br />
+          </form>
+        </div>
       </div>
-    );
-  };
-  
-  export default UpdatePatientInfo;
-  
+
+      <footer />
+    </div>
+  );
+}
+
+export default UpdatePatientInfo;
